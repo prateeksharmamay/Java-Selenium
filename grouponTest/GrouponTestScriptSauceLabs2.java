b4625660ed4748d357e32e6fff4fa4ed88652779
Prateek Sharma <prateek_sharma_may@yahoo.com>
2017-04-27 19:30:14 -0700

    Another script to run simultaneously on SauceLab Cluster

---
+++ grouponTest/GrouponTestScriptSauceLabs2.java
@@ -0,0 +1,42 @@
+package grouponTest;
+
+import static org.junit.Assert.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Scanner;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class GrouponTestScriptSauceLabs2 {
+	public static final String USERNAME = "prateeksharmamay";
+	public static final String ACCESS_KEY = "d8c23b6d-c5c4-4ecf-ad5c-a6060698a27d";
+	public static final String myURL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+	static WebDriver driver;
+	
+	public static void main(String args[]) throws MalformedURLException, InterruptedException{
+		
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+	    caps.setCapability("platform", "Windows XP");
+	    caps.setCapability("version", "43.0");
+	    
+	    WebDriver driver = new RemoteWebDriver(new URL(myURL), caps);
+	    Thread.sleep(5000);
+	    driver.get("https://saucelabs.com/test/guinea-pig");
+	    Thread.sleep(5000);
+	    System.out.println("title of page is: " + driver.getTitle());
+	    Thread.sleep(5000);
+	    driver.quit();
+	}
+}
